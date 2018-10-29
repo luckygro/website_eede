4e01f4d9366886eda7c0546552ed00c3ba11538b
luckygro <lukas@luckyg.de>
2018-10-29 22:05:32 +0100

    fix image size issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,81 @@
-import React from 'react'
-import { Link } from 'gatsby'
-
+import React, { Component } from 'react';
+import Contact from '../components/contact'
+import Img from 'gatsby-image'
+import { graphql } from "gatsby"
 import Layout from '../components/layout'
+import styled from 'styled-components'
+import Remarkable from 'remarkable'
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+
+// Styled Components
+
+const Article = styled.div`
+  margin: 20px 0;
+`;
+
+const Heading = styled.h1`
+  font-size: 40px;
+  color: #666;
+  font-family: Roboto, Arial;
+  font-weight: normal;
+`
+
+// render
+
+var md = new Remarkable({
+  html: true,
+  breaks: true
+});
+
+const Contacts = ({contacts}) => (
+  <div>
+    {contacts.map(singlecontact => (
+      <Contact key={singlecontact.name} node={singlecontact} />
+    ))}
+  </div>
+);
+
+class IndexPage extends Component {
+
+  render() {
+
+    const {
+      hauptslogan,
+      unterslogan,
+      heroImage,
+    } = this.props.data.contentfulStartPage
+
+
+    return (
+
+      <Layout>
+
+        <Heading>{hauptslogan}</Heading>
+        <h2>{unterslogan}</h2>
+
+        <Img sizes={heroImage.sizes} />
+
+      </Layout>
+    )
+  }
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query indexPageQuery {
+    contentfulStartPage(slug: {eq: "index"}) {
+      hauptslogan
+      unterslogan
+      heroImage {
+        file {
+          fileName
+          url
+        }
+        sizes(maxWidth: 1180, background: "rgb:000000") {
+          ...GatsbyContentfulSizes_tracedSVG
+        }
+      }
+    }
+  }
+`
