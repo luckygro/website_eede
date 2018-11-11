e4e6c9261fc5d3daa6ab3a81073a6d80d4205c9a
luckygro <lukas@luckyg.de>
2018-11-11 20:40:33 +0100

    add static contact form and backend function

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -4,7 +4,6 @@ import ReactMarkdown from 'react-markdown'
 import Img from 'gatsby-image'
 import { StaticQuery, graphql } from "gatsby"
 
-
 // Styled Components
 
 const PersonBox = styled.div`
@@ -16,13 +15,108 @@ const PersonImg = styled(Img)`
   border-radius: 50%;
 `
 
-const PersonName = styled.p`
-  font-weight: bold;
-  color: #666;
-  text-align: center;
+const InputGroupBig = styled.div`
+  float: left;
+  width: 100%;
 `
 
-const Contactform = () => {return(null)}
+const InputGroupSmall = styled.div`
+  float: left;
+  width: 100%;
+
+  @media (min-width: 1024px) {
+    width: 47%;
+    margin-left: 6%;
+  }
+  :nth-child(2) {
+    margin-left: 0%;
+  }
+
+`
+
+const Label = styled.label`
+  margin-top: 30px;
+  display: block;
+`
+
+const TextBox = styled.input`
+  display: block;
+  width: 100%;
+  background-color: #fff;
+  font-size: 16px;
+  padding: 10px;
+  margin: 10px 0px;
+  :focus {
+    border-left: #666 2px solid;
+  }
+  :invalid {
+    border-left: #a00 2px solid;
+  }
+  :valid {
+    border-left: #0a0 2px solid;
+  }
+`
+
+const TextArea = styled.textarea`
+  display: block;
+  width: 100%;
+  background-color: #fff;
+  border-left: #666 1px solid;
+  font-size: 16px;
+  padding: 10px 10px;
+  margin: 10px 0px;
+  :focus {
+    border-left: #666 2px solid;
+  }
+  :invalid {
+    border-left: #a00 2px solid;
+  }
+  :valid {
+    border-left: #0a0 2px solid;
+  }
+`
+
+const FormButton = styled.button`
+  display: block;
+  color: #fff;
+  font-size: 1em;
+  padding: 10px;
+  text-decoration: None;
+  margin-top: 30px;
+  background: #999;
+
+  :hover, :focus, :active {
+    background-color: #666;
+  }
+`;
+
+
+const Contactform = () => {
+  return(
+    <form id="contactForm" action="https://us-central1-eede-d8ccf.cloudfunctions.net/addMessage">
+
+      <h3>Senden Sie uns eine E-Mail!</h3>
+      <InputGroupSmall className="small">
+        <Label>Name</Label>
+        <TextBox type="text" name="name" required="true" placeholder="Vorname Nachname" />
+      </InputGroupSmall>
+
+      <InputGroupSmall className="small">
+        <Label>Email</Label>
+        <TextBox type="email" name="email" required="true" placeholder="ich@example.de"/>
+      </InputGroupSmall>
+
+      <InputGroupBig className="big">
+        <Label>Nachricht</Label>
+        <TextArea name="message" required="true" placeholder="Hallo..." rows="7"/>
+      </InputGroupBig>
+
+      <InputGroupBig>
+        <FormButton type="submit">Absenden</FormButton>
+      </InputGroupBig>
+    </form>
+  )
+}
 
 // tbd
 
